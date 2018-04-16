09ce18c3d020df14941b5af1d850b45b3b4e9bc2
Sam Vanderlinden <samvanderlinden6@gmail.com>
2018-04-16 14:23:38 -0500

    generate new div when generate button clicked, swap colors when swap button clicked and delete div when delete button clicked

--- client.js
+++ client.js
@@ -1,7 +1,42 @@
 console.log('javascript sourced');
 
+let counting = 0;
+
 $(document).ready(onReady);
 
 function onReady() {
     console.log('jquery sourced');
+    $('#generate-btn').on('click', clickedHandler);
+}
+
+function clickedHandler() {
+    console.log('Button has been clicked');
+    counting++;
+    console.log(counting);
+    $('body').append('<div class="contents">' + '<p class="counter">' + '</p>' + '</div>');
+    addButtons();
+    updateCounter();
+    remove();
+    swapColor();
+}
+
+function addButtons() {
+    $('.contents').append('<button class="swap-btn">' + 'Swap' + '</button');
+    $('.contents').append('<button class="delete-btn">' + 'Delete' + '</button');
+}
+
+function updateCounter() {
+    $('.counter').text(counting);
+}
+
+function swapColor() {
+    $('.swap-btn').on('click', function(){
+        $(this).parent().toggleClass('yellow');
+    });
+}
+
+function remove() {
+    $('.delete-btn').on('click', function(){
+        $(this).parent().remove();
+    });
 }
